f1ebd6c79f5040faa85e4a5c33360d333cd94427
mark <discordlalala17@gmail.com>
2024-01-12 19:26:02 +0700

    Iron Stick adding (part 1) 🔋

--- src/main/java/net/ecler/slepoikot/items/ModItems.java
+++ src/main/java/net/ecler/slepoikot/items/ModItems.java
@@ -1,18 +1,30 @@
 package net.ecler.slepoikot.items;
 
 import net.ecler.slepoikot.Slepoikot;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registries;
 import net.minecraft.item.Item;
 public class ModItems {
+    public static final Item IRONSTICK = registerItem("iron_stick", new Item(new FabricItemSettings()));
 
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries)
+    {
+     entries.add(IRONSTICK)
+    }
 
+    // Item register
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Slepoikot.MOD_ID, name), item);
     }
     public static void registerModItems() {
         Slepoikot.LOGGER.info(Slepoikot.MOD_ID + "Registering Mod Items");
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
     }
 
 }
