d67e8d963d2594b4cf11c12a6f11703f8e98c74d
mark <discordlalala17@gmail.com>
2024-01-12 18:20:57 +0700

    Added register for items!⤴️

---
+++ src/main/java/net/ecler/slepoikot/Slepoikot.java
@@ -0,0 +1,17 @@
+package net.ecler.slepoikot;
+
+import net.ecler.slepoikot.items.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slepoikot implements ModInitializer {
+    public  static final String MOD_ID = "slepoikot";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+	}
+}
---
+++ src/main/java/net/ecler/slepoikot/items/ModItems.java
@@ -0,0 +1,11 @@
+package net.ecler.slepoikot.items;
+
+import net.ecler.slepoikot.Slepoikot;
+
+public class ModItems {
+
+    public static void registerModItems() {
+        Slepoikot.LOGGER.info(Slepoikot.MOD_ID + "Registering Mod Items");
+    }
+
+}
