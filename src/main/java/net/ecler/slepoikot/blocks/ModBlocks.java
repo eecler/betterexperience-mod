49d8570a9b4c03374f2aa1db5613ac915c477f6c
mark <discordlalala17@gmail.com>
2024-01-14 16:46:40 +0700

    Registering Blocks 🧮📜

--- src/main/java/net/ecler/slepoikot/blocks/ModBlocks.java
+++ src/main/java/net/ecler/slepoikot/blocks/ModBlocks.java
@@ -1,8 +1,20 @@
 package net.ecler.slepoikot.blocks;
 
 import net.ecler.slepoikot.Slepoikot;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.registry.Registries;
+import net.minecraft.item.Item;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 
 public class ModBlocks {
+    private static Item registerBlockItem(String name, Block block) {
+      return Registry.register(Registries.ITEM, new Identifier(Slepoikot.MOD_ID, name),
+          new BlockItem(block, new FabricItemSettings()));
+    }
 
     public static void registerModBlocks() {
         Slepoikot.LOGGER.info(Slepoikot.MOD_ID + " Registering Mod Blocks");
